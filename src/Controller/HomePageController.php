64143800c4b38670b895cbfe5d61d23e251ff752
TYrueouJeL <guerinremi.pro@gmail.com>
2024-11-27 15:41:52 +0100

    requete sql homepage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\UnitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(): Response
+    public function index(UnitRepository $unitRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
+            'unitList' => $unitRepository->findAll(),
         ]);
     }
 }
