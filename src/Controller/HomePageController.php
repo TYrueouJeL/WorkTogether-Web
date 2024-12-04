d27e92770583b6d6ba94aedfe7731026f0a6af31
TYrueouJeL <guerinremi.pro@gmail.com>
2024-12-04 11:18:31 +0100

    Affichage des packs sur la page d'accueil

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PackRepository;
 use App\Repository\UnitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomePageController extends AbstractController
 {
     #[Route('/', name: 'app_home_page')]
-    public function index(UnitRepository $unitRepository): Response
+    public function index(PackRepository $packRepository): Response
     {
         return $this->render('home_page/index.html.twig', [
-            'unitList' => $unitRepository->findAll(),
+            'packList' => $packRepository->findAll(),
         ]);
     }
 }
